54d084674dcf7e350820e814d05308ed38c42bb8
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-05-04 13:38:52 -0300

    add migrations e models

---
+++ src/models/Local.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("../database/connection");
+
+
+const Matricula = connection.define("matriculas", {
+    nome: {
+        type: DataTypes.STRING
+    },
+    descricao: {
+        type: DataTypes.STRING
+    },
+    localidade: {
+        type: DataTypes.STRING
+    },
+    coordenadas_geograficas: {
+        type: DataTypes.STRING
+    },
+    usuarios_id: {
+        type: DataTypes.INTEGER
+    },
+    coordenadas_geograficas: {
+        type: DataTypes.STRING
+    },
+    createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+    },
+    updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+    }
+})
+
+module.exports = Matricula
