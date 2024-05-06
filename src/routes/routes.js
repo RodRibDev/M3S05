cabb77ee8ff15f437003c67982a3c7c4a196022e
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-05-06 12:55:42 -0300

    add arquivo login.route.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const usuarioRoutes = require("./usuarios.route");
+const loginRoutes = require("./login.route");
 
 const routes = Router()
 
 routes.use("/usuario", usuarioRoutes)
-
+routes.use("/login", loginRoutes)
 
 
 module.exports = routes
