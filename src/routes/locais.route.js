d819c9950312d94bb05daf861f0dac9da564d66f
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-05-11 19:13:50 -0300

    Add as rotas Atualizar e Deletar local no arquivo locais.route.js

--- src/routes/locais.route.js
+++ src/routes/locais.route.js
@@ -5,26 +5,6 @@ const { verify } = require('jsonwebtoken')
 
 const localRoutes = new Router()
 
-// async function uId (req, res, next) {
-//     const token = req.headers.authorization;
-
-//     if (!token) {
-//         console.error(error, error)
-//         return res.status(401).json({ message: "Token não é válido" });
-//     }
-
-//     try {
-//         const decodedToken = verify(token, process.env.SECRET_JWT);
-//         req.id = decodedToken.sub;
-//         console.log(req.id)
-//         next();
-
-//     } catch (error) {
-//          console.error("Erro ao verificar o token JWT:", error);
-//          return res.status(401).json({ error: "Acesso negado"})
-// }
-// }
-
 //Criar Novo Local
 localRoutes.post("/", auth, async(req, res) => {
 
@@ -35,14 +15,12 @@ localRoutes.post("/", auth, async(req, res) => {
         const coordenadas_geograficas = req.body.coordenadas_geograficas
         const usuarios_id = req.body.usuarios_id
     
-
         const local = await Local.create({
             nome: nome,
             descricao: descricao,
             localidade: localidade,
             coordenadas_geograficas: coordenadas_geograficas,
-            usuarios_id : usuarios_id
-                       
+            usuarios_id : usuarios_id             
         })
         res.json(local)
     } catch (error) {
@@ -74,6 +52,125 @@ localRoutes.get("/", auth, async (req, res) => {
         })
         res.status(200).json({listarPorId})
 
+    } catch (error) {
+         console.error("Erro ao validar o token JWT:", error);
+         return res.status(401).json({ error: "Acesso negado"})
+    }
+})
+
+
+// Listar informações detalhadas de um local específico
+localRoutes.get("/:id", auth, async(req, res) => {
+
+    try {
+        const { id } = req.params
+
+        const token = req.headers.authorization;
+
+        if (!token) {
+            console.error(error, error)
+            return res.status(401).json({ message: "Token não é válido" });
+        }
+
+        const Token = verify(token, process.env.SECRET_JWT);
+        req.id = Token.sub;
+
+        const listarLocal = await Local.findOne({
+            where: {
+                id: id,
+                usuarios_id: Token.sub
+            }
+        })
+
+        if (listarLocal){
+            res.status(200).json({listarLocal})  
+        } else {
+            res.status(400).json({message: "não existe local cadastrado neste ID"})
+        }
+    } catch (error) {
+         console.error("Erro ao validar o token JWT:", error);
+         return res.status(401).json({ error: "Acesso negado"})
+    }
+})
+
+
+// Atualizar local
+localRoutes.put("/:local_id", async(req, res) => {
+
+    try {
+        let { local_id } = req.params
+
+        const nome = req.body.nome
+        const descricao = req.body.descricao
+        const localidade = req.body.localidade
+        const coordenadas_geograficas = req.body.coordenadas_geograficas
+        const usuarios_id = req.body.usuarios_id
+
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ message: "Token não é válido" });
+        }
+
+        const Token = verify(token, process.env.SECRET_JWT);
+        req.id = Token.sub;
+
+        const editLocal = await Local.findOne({
+            where: {
+                id: local_id,
+                usuarios_id: Token.sub
+            }
+        })
+
+        if (editLocal){
+            editLocal.update({
+                nome : nome, 
+                descricao : descricao, 
+                localidade : localidade,
+                coordenadas_geograficas : coordenadas_geograficas,
+                usuarios_id : usuarios_id
+            })
+                
+            await editLocal.save()
+
+            res.status(200).json({message:"Local atualizado com sucesso!"})  
+        } else {
+            res.status(400).json({message: "não existe local cadastrado neste ID"})
+        }
+    } catch (error) {
+         console.error("Erro ao validar o token JWT:", error);
+         return res.status(401).json({ error: "Acesso negado"})
+    }
+})
+
+
+// Deletar local
+localRoutes.delete("/:local_id", auth, async(req, res) => {
+    try {
+        const { local_id } = req.params
+
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ message: "Token não é válido" });
+        }
+
+        const Token = verify(token, process.env.SECRET_JWT);
+        req.id = Token.sub;
+
+        const local = await Local.findOne({
+            where: {
+                id: local_id,
+                usuarios_id: Token.sub
+            }
+        })
+
+        if (local){
+            local.destroy()
+            res.status(204).json({})  
+        } else {
+            res.status(400).json({message: "Local não encontrado"})
+        }
 
     } catch (error) {
          console.error("Erro ao validar o token JWT:", error);
