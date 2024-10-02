17f20f897c7af48d564aa0eeaefa9ecc71afd2f9
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2024-10-01 23:14:38 -0300

    atualização das validações de campos obrigatórios

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -4,12 +4,24 @@ const { connection } = require('../database/connection')
 const Usuario = connection.define('usuarios', {
     email:{
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     password:{
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     nome: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     cpf: {
         type: DataTypes.STRING(11),
@@ -22,12 +34,24 @@ const Usuario = connection.define('usuarios', {
     },
     sexo: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     endereco: {
         type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     },
     data_nascimento: {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
     }
     
 })
