820ba08cc6ff033e38bc7f8ad5238e5a7f77a0af
leilianelcs <leilianelcs@gmail.com>
2024-10-05 02:27:35 -0300

    Atualizando seeders para integração Front-End e Back-End

--- src/database/seeders/Locais.seeder.js
+++ src/database/seeders/Locais.seeder.js
@@ -7,15 +7,25 @@ module.exports = {
             {
                 nome: "Parque Botânico de Florianópolis",
                 descricao: "Grande parque que contém arvores enormes, locais de plantação diversificada e um ótimo espaço para piquiniques",
-                localidade: "Localizado no bairro Itacorubi",
                 cep: "88034000",
+                rua: "Rua ABC",
+                cidade: "Florianópolis",
+                bairro: "Centro",
+                uf: "SC",
+                latitude: "-27.39652",
+                longitude: "-48.39652",
                 usuarios_id: "100"
             },
             {
                 nome: "Trilha do Gravatá",
                 descricao: "Ambiente misto de belas trilhas naturais e paisagens incríveis",
-                localidade: "Localizado em Lagoa da Conceição",
                 cep: "88062400",
+                rua: "Rua da Praia",
+                cidade: "Florianópolis",
+                bairro: "Gravatá",
+                uf: "SC",
+                latitude: "-30.39652",
+                longitude: "-60.39652",
                 usuarios_id: "101"
             }
         ])
